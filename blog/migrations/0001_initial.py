2aad07bb52812042a8d9cb2d112b21b96b25ab9b
John Rees <johnleonardrees@gmail.com>
2017-02-03 14:14:09 +0000

    Adding existing files
    Adding existing files

---
+++ blog/migrations/0001_initial.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.utils.timezone
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
+                ('title', models.CharField(max_length=250)),
+                ('slug', models.SlugField(unique_for_date='publish', max_length=250)),
+                ('body', models.TextField()),
+                ('publish', models.DateTimeField(default=django.utils.timezone.now)),
+                ('create', models.DateTimeField(auto_now=True)),
+                ('status', models.CharField(default='draft', choices=[('draft', 'Draft'), ('published', 'Published')], max_length=10)),
+                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='blog_posts')),
+            ],
+            options={
+                'ordering': ('-publish',),
+            },
+        ),
+    ]
