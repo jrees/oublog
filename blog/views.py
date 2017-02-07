91588e1fdc12a8b9d05322d5ef8ad976a08547fa
John Rees <johnleonardrees@gmail.com>
2017-02-07 16:29:43 +0000

    Revert "Added 'sahre this post'"
    This reverts commit a370eb00b7e8630d290cb246088e5378fec5ccf3.

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.views.generic import ListView
-from django.core.mail import send_mail
 from .models import Post
-from .forms import EmailPostForm
-
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
 
 # Create your views here.
 def post_list(request):
@@ -38,25 +29,3 @@ def post_detail(request, year, month, day, post):
     return render(request,
         'blog/post/detail.html',
         {'post': post})
-
-def post_share(request, post_id):
-    # retireve post by id
-    post = get_object_or_404(Post, id=post_id, status='published')
-    sent = False
-
-    if request.method == 'POST':
-        # Form was submitted
-        if form.is_valid():
-            # Form fields passed validation
-            cd = form.cleaned_data
-            # send Email
-            post_url = request.build_absolute_uri(post.get_absolute_url())
-            subject = '{} ({}) recommends you reading "{}"'.\
-            format(cd['name'], cd['email'], post.title)
-            message = 'Read "{}" at {}\n\n{}\'s comments: {}'.\
-            format(post.title, post_url, cd['name'], cd['comments'])
-            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
-            sent = True
-    else:
-        form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post, 'form':form, 'sent': sent})
